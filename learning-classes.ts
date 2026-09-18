4042fc780df554145d08b72c26dadd3b475801ac
storyofwoe <julietmewett@icloud.com>
2026-09-18 14:10:44 +0100

    added read only fields, super calls

--- learning-classes.ts
+++ learning-classes.ts
@@ -14,7 +14,7 @@ class Point {
 }
 
 const pt = new Point();
-//pt.x = "0"; //Error
+//pt.x = "0"; //Type Error
 console.log(`(${pt.x}, ${pt.y})`);
 
 // Can also do:
@@ -30,3 +30,33 @@ class Point2 {
 const pt2 = new Point2();
 console.log(`(${pt2.x}, ${pt2.y})`);
 
+//read only fields
+class Greeter {
+    readonly name: string = "world"; //readonly prevents assignments outside of the constructor
+
+    constructor(otherName?: string) {
+        if (otherName !== undefined) {
+            this.name = otherName;
+        }
+    }
+}
+
+const g = new Greeter();
+//g.name = "Error";
+console.log(g.name);
+const g2 = new Greeter("Hello");
+console.log(g2.name);
+
+//super calls
+class Base {
+    k: number = 4;
+}
+
+class Derived extends Base {
+    constructor() {
+        super();
+        console.log(this.k);
+    }
+}
+
+const d = new Derived(); //output: 4
